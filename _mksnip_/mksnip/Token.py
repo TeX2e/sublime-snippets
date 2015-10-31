471ee61eba741d2b933e7a385c58647953eba709
Mako <mnfeconicu41@gmail.com>
2015-10-31 23:00:55 +0900

    add docstring in Token.py

--- _mksnip_/mksnip/Token.py
+++ _mksnip_/mksnip/Token.py
@@ -6,12 +6,19 @@ Token = collections.namedtuple('Token', ['typ', 'value', 'line', 'column'])
 
 
 class Tokens(object):
-	"""docstring for Tokens"""
+	"""
+	Create a new tokens from the given token list.
+	Tokens is composed of iterable items.
+	"""
 
 	__pos = 0;
 	# __elems = [Token(), ...]
 
 	def __init__(self, tokens):
+		"""
+		argument "tokens" is expected to be Token list such as:
+			[Token(..), Token(..), ..]
+		"""
 		self.__elems = tokens
 
 	@property
@@ -19,9 +26,11 @@ class Tokens(object):
 		return self.__elems
 
 	def __iter__(self):
+		""" Return self """
 		return self
 
 	def next(self):
+		""" Return next token, and increment iterator. """
 		try:
 			next_val = self.__elems[self.__pos]
 		except IndexError:
@@ -31,4 +40,5 @@ class Tokens(object):
 			return next_val
 
 	def seek(self):
+		""" Return next token. """
 		return self.__elems[self.__pos]
